2e7981b7cf6b360579872fbdb2d65ae9876db9d1
Jack <xhzengAIB@gmail.com>
2014-01-17 10:05:42 +0000

    新建model类

---
+++ Source/XHThumbnail.h
@@ -0,0 +1,13 @@
+//
+//  XHThumbnail.h
+//  XHThumbnailAnnotation
+//
+//  Created by 曾 宪华 on 14-1-17.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XHThumbnail : NSObject
+
+@end
